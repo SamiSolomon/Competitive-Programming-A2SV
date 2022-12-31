d99ce09be7955b6a3c2c9b602c2134b181e9355c
Samuel Solomon <112163271+SamiSolomon@users.noreply.github.com>
2022-12-31 05:39:39 -0800

    Create target indices after sorting array.py

---
+++ target indices after sorting array.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def targetIndices(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        output = []
+        nums.sort()
+        for i in range(len(nums)):
+            if nums[i] == target:
+                output.append(i)
+        return output
