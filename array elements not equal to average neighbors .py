96038150d3be7f19b3b069b108bd1cb46b4fdcaf
Samuel Solomon <112163271+SamiSolomon@users.noreply.github.com>
2023-01-05 00:32:48 -0800

    Create array elements not equal to average neighbors .py

---
+++ array elements not equal to average neighbors .py
@@ -0,0 +1,12 @@
+class Solution:
+    def rearrangeArray(self, nums: List[int]) -> List[int]:
+        nums.sort()
+        result =[]
+        l,r =0,len(nums) -1
+        while len(result) != len(nums):
+            result.append(nums[l])
+            l +=1
+            if l <=r:
+                result.append(nums[r])
+                r -=1
+        return result
